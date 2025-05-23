35337a4cce1768c7793da73b45985538118b3a6c
markitox <ma.sanmartinb@gmail.com>
2025-05-23 15:58:20 -0400

    Actualizacion ProductoController

--- src/main/java/com/example/EcoMarket/EcoMarketApplication.java
+++ src/main/java/com/example/EcoMarket/EcoMarketApplication.java
@@ -1,5 +1,6 @@
 package com.example.EcoMarket;
 
+import model.Producto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/controller/ProductoController.java
+++ src/main/java/controller/ProductoController.java
@@ -1,36 +1,52 @@
 package controller;
 
-
+import model.Cliente;
 import model.Producto;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ProductoController {
 
+    public List<Producto> ListaProducto = new ArrayList<Producto>();
+
+    public boolean agregarProducto(Producto newProducto) {
+            return ListaProducto.add(newProducto);
+    }
+
+
     public List<Producto> obtenerTodos() {
-        return productoService.obtenerTodos();
+        return ListaProducto;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Producto> obtenerPorId(@PathVariable int id) {
-        Optional<Producto> producto = productoService.obtenerPorId(id);
-        return producto.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-    }
-
-    public Producto crearProducto(@RequestBody Producto producto) {
-        return productoService.guardar(producto);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Producto> actualizarProducto(@PathVariable int id, @RequestBody Producto productoActualizado) {
-        Optional<Producto> producto = productoService.actualizar(id, productoActualizado);
-        return producto.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminarProducto(@PathVariable int id) {
-        if (productoService.eliminar(id)) {
-            return ResponseEntity.noContent().build();
+    //Actualizar
+    public boolean actualizarProducto(int id, int stock, String nombre, String categoria, float precio, Producto productoActualizado) {
+        for (int i = 0; i < ListaProducto.size(); i++) {
+            Producto producto = ListaProducto.get(i);
+            if (producto.getIdProducto() == id) {
+                ListaProducto.set(i, productoActualizado);
+                producto.setIdProducto(id);
+                producto.setStockProducto(stock);
+                producto.setNombreProducto(nombre);
+                producto.setCategoriaProducto(categoria);
+                producto.setPrecioProducto(precio);
+                return true;
+            }
         }
-        return ResponseEntity.notFound().build();
-}
+        return false; // Si no se encuentra, retornamos false
+    }
 
+    public boolean eliminarProducto(int id) {
+        for (int i = 0; i < ListaProducto.size(); i++) {
+            Producto producto = ListaProducto.get(i);
+            if (producto.getIdProducto() == id) {
+                ListaProducto.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
 }
